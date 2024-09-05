1b5aa38b188c4748a7339579d652fdaa6c0fd4d4
Nattkarn <30921254+nattkarn@users.noreply.github.com>
2024-09-05 18:29:59 +0700

    reset

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,63 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ProductsModule } from './products/products.module';
-import { OrdersModule } from './orders/orders.module';
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { MulterModule } from '@nestjs/platform-express';
-import { MulterOptions } from './config/upload.config';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
-import { MailerModule } from '@nestjs-modules/mailer';
-
-
 
 @Module({
-  imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..','uploads')
-      
-  }),
-    //เพิ่มไว้สำหรับอ่าน .env
-    ConfigModule.forRoot({
-      isGlobal:true,
-      envFilePath: '.env'
-    })
-    ,
-    MongooseModule.forRootAsync({
-      useFactory: async (configService: ConfigService) => ({
-        uri: configService.get<string>('MONGODB_URI')
-      }),
-      inject: [ConfigService],
-    }),
-    ProductsModule,
-    OrdersModule,
-    AuthModule,
-    UserModule,
-    MulterModule.register(MulterOptions),
-    MailerModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        transport: {
-          host: configService.get<string>('EMAIL_HOST'),
-          port: configService.get<number>('EMAIL_PORT'),
-          secure: false,
-          auth: {
-            user: configService.get<string>('EMAIL_USER'),
-            pass: configService.get<string>('EMAIL_PASSWORD'),
-          },
-        },
-      }),
-      inject: [ConfigService],
-    }),
-  ],
+  imports: [],
   controllers: [AppController],
-  providers: [AppService,
-    
-  ],
+  providers: [AppService],
 })
-
-export class AppModule { }
+export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,49 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import * as cookieParser from 'cookie-parser'
 import { AppModule } from './app.module';
-import { ValidationPipe, BadRequestException  } from '@nestjs/common';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const config = new DocumentBuilder()
-    .setTitle('NestJS Crud example')
-    .setDescription('The NestJS Crud API')
-    .setVersion('1.0')
-    .addTag('NestJS Crud')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-
-  // Enable CORS
-  app.enableCors(
-    {
-    origin: '*', // Allow all origins or specify the allowed origins
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Specify the allowed HTTP methods
-    credentials: true, // Enable cookies or HTTP authentication
-  }
-);
-    
-
-
-  app.use(cookieParser())
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true, // ตัด properties ของข้อมูลที่ส่งเข้ามาที่ไม่ได้นิยามไว้ใน dto ออกไป
-      forbidNonWhitelisted: true, // ตัวเลือกนี้จะทำงานคู่กับ whitelist โดยหากตั้งค่าเป็น true จะทำให้เกิด error ในกรณีนี้มี properties ใดที่ไม่ได้อยู่ใน whitelist ส่งเข้ามา
-      transform: true, // ตัวเลือกนี้ทำให้เกิดการแปลงชนิดข้อมูลอัตโนมัติ ในข้อมูลจากภายนอกให้ตรงกับชนิดที่นิยามไว้ใน DTO
-      exceptionFactory: (errors) => {
-        // ตัวเลือกนี้ทำให้สามารถกำหนดรูปแบบของ error response เมื่อการตรวจ validation ล้มเหลวได้
-        const messages = errors.map((error) => ({
-          field: error.property,
-          message: Object.values(error.constraints).join('. ') + '.',
-        }));
-        return new BadRequestException({ errors: messages });
-      },
-    }),
-  );
-  
-  await app.listen(3000);
+  await app.listen(5000);
 }
 bootstrap();
